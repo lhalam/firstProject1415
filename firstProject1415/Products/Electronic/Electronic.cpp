5bb652292a068e0989ed647cc553d5b1217e176a
BohdanZhmud <bohdan23111996@gmail.com>
2015-04-07 17:12:05 +0300

    initial commit

---
+++ firstProject1415/Products/Electronic/Electronic.cpp
@@ -0,0 +1,42 @@
+#include"Electronic.h"
+
+	Electronic::Electronic(): Product(), category("No category"), weight(0.0), model("No model") { }
+
+	void Electronic::input()
+	{
+		Product::input();
+		cout << Message("Input category: ", CONTEXT_MSG);
+		getline(cin, category);
+		cout << Message("Input weight: ", CONTEXT_MSG);
+		cin >> weight;
+		cin.get();
+		cout << Message("Input model: ", CONTEXT_MSG);
+		getline(cin, model);
+	}
+
+	void Electronic::output() const
+	{
+		Product::Output();
+		cout << Message("Category: " + category, INPUT_MSG);
+		cout << Message("Weight: " + to_string(weight), INPUT_MSG);
+		cout << Message("Model: " + model, INPUT_MSG);
+	}
+    
+    istream& operator>>(istream& stream, Electronic& obj)
+    {
+        getline(stream, obj.category);
+        stream >> obj.weight;
+        stream.get();
+        getline(stream, obj.model);
+        return stream;
+    }
+    
+    ostream& operator<<(ostream& stream, const Electronic& obj)
+    {
+        stream << "Category: " << obj.category << endl;
+		stream << "Weight: " << obj.weight << endl;
+		stream << "Model: " << obj.model << endl;
+        return stream;
+    }
+
+
