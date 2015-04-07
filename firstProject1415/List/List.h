ee29424a51716226555e0876bb3ab6d4df60f005
BohdanZhmud <bohdan23111996@gmail.com>
2015-04-07 22:01:00 +0300

    Added iterator

--- firstProject1415/List/List.h
+++ firstProject1415/List/List.h
@@ -1,27 +1,107 @@
 #pragma once
 
-template <typename T>
-struct Node
-{
-	Node* prev;
-	Node* next;
-	T value;
-	Node(const T& value_ = T(), Node* next_ = nullptr, Node* prev_ = nullptr) :
-		value(value_), next(next_), prev(prev_){}
-};
-
 template<typename T>
 class List
 {
 private:
-	Node<T>* head;
-	Node<T>* tail;
+	struct Node
+	{
+		Node* prev;
+		Node* next;
+		T value;
+		Node(const T& value_ = T(), Node* next_ = nullptr, Node* prev_ = nullptr) :
+			value(value_), next(next_), prev(prev_){}
+	};
+
+	Node* head;
+	Node* tail;
+	Node* emptyNode;
 	int s;
 
 public:
-	List() : head(nullptr), tail(nullptr), s(0)
+	List() : head(nullptr), tail(nullptr), emptyNode(new Node()), s(0)
 	{
 
+	}
+
+	class iterator
+	{
+	private:
+		Node* ptr;
+
+	public:
+		friend class List;
+		iterator() : ptr(nullptr)
+		{
+
+		}
+
+		explicit iterator(Node* ptr_) : ptr(ptr_)
+		{
+
+		}
+
+		iterator(const iterator& copy) : ptr(copy.ptr)
+		{
+
+		}
+
+		T& operator*()
+		{
+			return ptr->value;
+		}
+
+		const T& operator*() const
+		{
+			return ptr->value;
+		}
+
+		iterator operator++()
+		{
+			ptr = ptr->next;
+			return *this;
+		}
+
+
+		iterator operator++(int)
+		{
+			ptr = ptr->next;
+			return *this;
+		}
+
+		iterator operator--()
+		{
+			ptr = ptr->prev;
+			return *this;
+		}
+
+		iterator operator--(int)
+		{
+			ptr = ptr->prev;
+			return *this;
+		}
+
+		bool operator==(const iterator& cmp)
+		{
+			return ptr == cmp.ptr;
+		}
+
+		bool operator!=(const iterator& cmp)
+		{
+			return ptr != cmp.ptr;
+		}
+	};
+
+	//returns iterator to first element
+	iterator begin() const
+	{
+		return iterator(head);
+	}
+
+	//returns iterator to element after last one
+	iterator end() const
+	{
+		return iterator(emptyNode);
 	}
 
 	~List();
@@ -40,11 +120,29 @@ public:
 		return tail->value;
 	}
 
-	Node<T>* find(const T& value, Node<T>* startPtr = nullptr, Node<T>* endPtr = nullptr) const;
+	//returns iterator containing value to find
+	//uses interval to find value (gets also iterator to begin, and iterator to element after last on interval)
+	iterator find(const T& value, const iterator& startSearch, const iterator& endSearch) const
+	{
+		iterator iterToSearch = startSearch;
 
-	void insert(Node<T>*, const T&);
+		do
+		{
+			if (*iterToSearch == value)
+			{
+				return iterToSearch;
+			}
+			iterToSearch++;
+		} while (iterToSearch != endSearch);
 
-	void erase(Node<T>*);
+		return endSearch;
+	}
+
+	//inserts value before element in list
+	void insert(iterator&, const T&);
+
+	//removes element
+	void erase(iterator& );
 
 	int size() const
 	{
@@ -52,13 +150,30 @@ public:
 	}
 };
 
+template<typename T>
+iterator find(const T& value, const iterator& startSearch, const iterator& endSearch) const
+{
+	iterator iterToSearch = startSearch;
+
+	do
+	{
+		if (*iterToSearch == value)
+		{
+			return iterToSearch;
+		}
+		iterToSearch++;
+	} while (iterToSearch != endSearch);
+
+	return endSearch;
+}
+
+
 template <typename T>
 List<T>::~List()
 {
-	Node<T> *ptrToDel = head;
 	while (head)
 	{
-		Node<T> *ptrToDel = head;
+		Node* ptrToDel = head;
 		head = head->next;
 		delete ptrToDel;
 	}
@@ -70,14 +185,18 @@ void List<T>::pushBack(const T & value)
 	s++;
 	if (tail)
 	{
-		tail->next = new Node<T>(value, nullptr, tail);
+		tail->next = new Node(value, nullptr, tail);
 		tail = tail->next;
+		tail->next = emptyNode;
 	}
 
 	else
 	{
-		tail = head = new Node<T>(value);
+		tail = head = new Node(value);
+		tail->next = emptyNode;
 	}
+
+	emptyNode->prev = tail;
 }
 
 template <typename T>
@@ -86,70 +205,60 @@ void List<T>::pushFront(const T & value)
 	s++;
 	if (head)
 	{
-		head->prev = new Node<T>(value, head, head->next);
+		head->prev = new Node(value, head, head->next);
 		head = head->prev;
 	}
 	else
 	{
-		tail = head = new Node<T>(value);
+		tail = head = new Node(value);
 	}
 }
 
 template <typename T>
-Node<T>* List<T>::find(const T& value, Node<T>* startPtr, Node<T>* endPtr) const
+void List<T>::insert(iterator& iterToInsert, const T& value)
 {
-	if (!startPtr) startPtr = head;
-
-	Node<T> *ptrToSearch = startPtr;
-
-	do
-	{
-		if (ptrToSearch->value == value)
-		{
-			return ptrToSearch;
-		}
-		ptrToSearch = ptrToSearch->next;
-	} while (ptrToSearch != endPtr);
-
-	return nullptr;
-}
-
-template <typename T>
-void List<T>::insert(Node<T>* ptrToInsert, const T& value)
-{
-	if (ptrToInsert == head)
+	if (iterToInsert == begin())
 	{
 		pushFront(value);
 		return;
 	}
 
-	Node<T>* insertNode = new Node<T>(value, ptrToInsert);
+	else if (iterToInsert == end())
+	{
+		pushBack(value);
+		return;
+	}
 
-	ptrToInsert->prev->next = insertNode;
-	insertNode->prev = ptrToInsert->prev;
-	ptrToInsert->prev = insertNode;
+	Node* insertNode = new Node(value, iterToInsert.ptr);
+
+	iterToInsert.ptr->prev->next = insertNode;
+	insertNode->prev = iterToInsert.ptr->prev;
+	iterToInsert.ptr->prev = insertNode;
 }
 
 template <typename T>
-void List<T>::erase(Node<T>* ptrToErase)
+void List<T>::erase(iterator& iterToDel)
 {
-	if (ptrToErase != head)
+	if (iterToDel.ptr != head)
 	{
-		ptrToErase->prev->next = ptrToErase->next;
+		iterToDel.ptr->prev->next = iterToDel.ptr->next;
 	}
 	else
 	{
-		head = ptrToErase->next;
+		head = iterToDel.ptr->next;
 	}
-	if (ptrToErase != tail)
+	if (iterToDel.ptr != tail)
 	{
-		ptrToErase->next->prev = ptrToErase->prev;
+		iterToDel.ptr->next->prev = iterToDel.ptr->prev;
 	}
 	else
 	{
-		tail = ptrToErase->prev;
+		tail = iterToDel.ptr->prev;
+		emptyNode->prev = tail;
 	}
-	delete ptrToErase;
+	delete iterToDel.ptr;
+	iterToDel.ptr = nullptr;
 }
+
 
 
