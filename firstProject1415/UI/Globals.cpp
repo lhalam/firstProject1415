f5aac27f67f4373cd2553b3f0fca154d8bedc542
Marakaido <yaroslav.mykoliv28@gmail.com>
2015-04-07 17:26:56 +0300

    Added file 'Globals.cpp' for global variables initialization

---
+++ firstProject1415/UI/Globals.cpp
@@ -0,0 +1,3 @@
+#include "Globals.h"
+
+User currentUser = User();
--- firstProject1415/UI/Globals.h
+++ firstProject1415/UI/Globals.h
@@ -1,4 +1,5 @@
 #pragma once
 #include "../User/User.h"
+
 /* Declarations of all global variables (all extern) */
 extern User currentUser;
