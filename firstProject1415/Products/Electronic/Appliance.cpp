6eb496c0e8a0a805daad46a7c6b0226a9ef97de7
BohdanZhmud <bohdan23111996@gmail.com>
2015-04-07 18:05:35 +0300

    Changed input/output methods

--- firstProject1415/Products/Electronic/Appliance.cpp
+++ firstProject1415/Products/Electronic/Appliance.cpp
@@ -1,35 +1,37 @@
-#include "Appliance.h"
+#include "AudioAndTv.h"
 
-Appliance::Appliance() : Electronic(), memory_card(0) {}
+AudioAndTv::AudioAndTv() : Electronic(), memory_card(0) {}
 
-friend istream& operator>>(istream& stream, Appliance& obj)
+friend istream& operator>>(istream& stream, AudioAndTv& obj)
 {
 	getline(stream, obj.category);
 	stream >> obj.weight;
 	stream.get();
 	getline(stream, obj.model);
 	stream >> obj.memory_card;
+	stream.get();
 	return stream;
 }
 
-friend ostream& operator<<(ostream& stream, const Appliance& obj)
+friend ostream& operator<<(ostream& stream, const AudioAndTv& obj)
 {
-	stream << obj.category << endl 
-		   << obj.weight << endl 
-		   << obj.model << endl 
-		   << obj.memory_card << endl;
+	stream << obj.category << endl
+		<< obj.weight << endl
+		<< obj.model << endl
+		<< obj.memory_card << endl;
 	return stream;
 }
 
-void Appliance::input()
+void AudioAndTv::input()
 {
 	Electronic::input();
 	cout << Message("Input memory card: ", CONTEXT_MSG);
-	cin >> this->memory_card;
+	cin >> memory_card;
+	cin.get();
 }
 
-void Appliance::output() const
+void AudioAndTv::output() const
 {
 	Electronic::output();
-	cout << Message("Memory card: " + this->memory_card, INPUT_MSG);
+	cout << Message("Memory card: " + to_string(memory_card), INPUT_MSG);
 }
