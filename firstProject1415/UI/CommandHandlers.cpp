d6ab682e47bba0ff2ea70691412404e9c922fe20
UlyanaAndrukhiv <ullann1ylaa@gmail.com>
2015-04-14 23:50:43 +0300

    Update CommandHandlers.cpp

--- firstProject1415/UI/CommandHandlers.cpp
+++ firstProject1415/UI/CommandHandlers.cpp
@@ -123,12 +123,6 @@ Result logIn()
 	}
 	else
 	{
-		currentUser = *existingUser;
-		currentUser->getLogin();
-		currentUser->getPassword();
-		currentUser->getId();
-		currentUser->getRole();
-		currentUser->getEmail();
 		cout << Message("Authentication successful\nWelcome, " + name + "!", LOG_MSG);
 	}
 	return Result();
