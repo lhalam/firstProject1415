d160cce802001e388765f7a9f543c7f290608bbb
NastyaKolomoets <nastya.kolomoeys@gmail.com>
2015-04-15 00:18:46 +0300

    Update Accessory.cpp

--- firstProject1415/Products/Textile/Accessory.cpp
+++ firstProject1415/Products/Textile/Accessory.cpp
@@ -8,26 +8,13 @@ Accessory::Accessory() :
 // overloading operators istream and ostream 
 istream& operator>>(istream& stream, Accessory& obj)
 {
-	getline(stream, obj.name);
-	getline(stream, obj.manufacturer);
-	stream >> obj.price >> obj.id;
-	getline(stream, obj.material);
-	stream >> obj.category;
-	getline(stream, obj.manufacturer);
-	stream >> obj.collectionYear;
+	stream >> Textile(obj);
 	return stream;
 }
 
 ostream& operator<<(ostream& stream, Accessory& obj)
 {
-	stream << obj.name << endl
-		<< obj.manufacturer << endl
-		<< obj.price << endl
-		<< obj.id << endl
-		<< obj.material << endl
-		<< obj.category << endl
-		<< obj.brand << endl
-		<< obj.collectionYear << endl;
+	stream << Textile(obj);
 	return stream;
 }
 
