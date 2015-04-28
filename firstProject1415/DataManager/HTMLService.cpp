5b9d39b425eda2c947f85310cc1665648a329004
Myroslav <vmv2580@gmail.com>
2015-04-28 13:48:58 +0300

    Coments added

--- firstProject1415/DataManager/HTMLService.cpp
+++ firstProject1415/DataManager/HTMLService.cpp
@@ -5,6 +5,7 @@ using namespace std;
 
 void HTMLService::write(const Date& start, const Date& end)
 {
+	//Change the direction if you want
 	ofstream file("File.html");
 	file << "<!DOCTYPE html>\n";
 	file << "<html>\n";
@@ -12,11 +13,12 @@ void HTMLService::write(const Date& start, const Date& end)
 	file << "<head>";
 	file << "</head>\n";
 
+	//Change cycle as you wish
 	file << "<body>\n";
-	/*for (unsigned i = 0; i < 5; i++)
+	for (unsigned i = 0; i < 5; i++)
 	{
-		file << "<p>" << i <<"</p>";
-	}*/
+		//file << "<p>" << i <<"</p>";
+	}
 	file << "</body>\n";
 
 	file << "</html>";
