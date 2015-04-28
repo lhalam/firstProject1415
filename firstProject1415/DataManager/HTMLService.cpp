3349ada30c882892c6213af447002f53869c120d
JuliaZvizlo <zvjuli24@gmail.com>
2015-04-28 14:08:42 +0300

    Created head tag and empty style tag

--- firstProject1415/DataManager/HTMLService.cpp
+++ firstProject1415/DataManager/HTMLService.cpp
@@ -1,17 +1,22 @@
 #include "HTMLService.h"
 #include <fstream>
+#include <string>
 
 using namespace std;
 
 void HTMLService::write(const Date& start, const Date& end)
 {
 	//Change the direction if you want
-	ofstream file("File.html");
+	ofstream file("History.html");
 	file << "<!DOCTYPE html>\n";
 	file << "<html>\n";
 
-	file << "<head>";
-	file << "</head>\n";
+	string head = "<head>\n"
+				  "<title>Purchase history</title>\n"
+				  "<meta charset=\"UTF-8\"/>\n" 
+				  "<style type=\"text/css\">\n"
+				  "</style>\n"
+				  "</head>";
 
 	//Change cycle as you wish
 	file << "<body>\n";
