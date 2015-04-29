113879f31aaf3824ffc33e02c4cbcba1ca355f97
Mike <tmikent@gmail.com>
2015-04-29 03:36:39 +0300

    Simpler commands

--- firstProject1415/UI/Command.cpp
+++ firstProject1415/UI/Command.cpp
@@ -5,12 +5,12 @@
 /*Initialization of commands; commands are listed alphabetically*/
 Command commands[] = 
 { 
-	Command("add product", addNewProduct, "adds new product", Access::ADMIN),
-	Command("add product to cart", addProductToCart, "add product to cart by id ", Access(GUEST | USER)),
+	Command("add -p", addNewProduct, "adds new product", Access::ADMIN),
+	Command("add -pc", addProductToCart, "add product to cart by id ", Access(GUEST | USER)),
 	Command("buy", buyOneElementById, "buys product by id", Access::USER),
-	Command("buy all products", buyAllProductFromCart, "buys all product from cart", Access(USER | GUEST)),
+	Command("buy -a", buyAllProductFromCart, "buys all product from cart", Access(USER | GUEST)),
 	Command("change amount", changeAmount, "changes amount of products", Access::ADMIN),
-	Command("change product", changeProduct, "changes product settings", Access::ADMIN),
+	Command("change -p", changeProduct, "changes product settings", Access::ADMIN),
 	Command("exit", exit, "exits the program", Access(GUEST | USER | ADMIN)),
 	Command("export -p", exportProdXML, "exports all existing products to XML file", Access::ADMIN),
 	Command("help", help, "provides you with the list of available commands and their description", Access(GUEST | USER | ADMIN)),
@@ -19,15 +19,15 @@ Command commands[] =
 	Command("log out", logOut, "exits user profile", Access(USER | ADMIN)),
 	Command("register", createUser, "creates a new account", Access(GUEST | ADMIN)),
 	Command("register admin", createAdmin, "makes a normal user an administrator", Access::ADMIN),
-	Command("remove user", removeUser, "deletes a specified user account", Access::ADMIN),
-	Command("remove product", removeProductFromAssortment, "removes the product from assortment", Access::ADMIN),
-	Command("remove product from cart", removeProductFromCart, "deletes a product from your cart", Access(GUEST | USER)),
-	Command("show all products", showProducts, "shows the list of all products", Access(GUEST | USER | ADMIN)),
-	Command("show all users", showUsers, "shows the list of all users", Access::ADMIN),
-	Command("show cart", showCart, "shows all products added to cart", Access(GUEST | USER)),
-	Command("show purchase history", showPurchaseHistory, "shows the history of the purchases of the user", Access(USER | ADMIN)),
-	Command("show stats", showStats, "shows the statistics of all bought products", Access::ADMIN),
-	Command("add to cart", addProductToCart ,"add product to cart by id ", Access(GUEST | USER | ADMIN)),
+	Command("remove -u", removeUser, "deletes a specified user account", Access::ADMIN),
+	Command("remove -p", removeProductFromAssortment, "removes the product from assortment", Access::ADMIN),
+	Command("remove -pc", removeProductFromCart, "deletes a product from your cart", Access(GUEST | USER)),
+	Command("show -ap", showProducts, "shows the list of all products", Access(GUEST | USER | ADMIN)),
+	Command("show -au", showUsers, "shows the list of all users", Access::ADMIN),
+	Command("show -c", showCart, "shows all products added to cart", Access(GUEST | USER)),
+	Command("show -h", showPurchaseHistory, "shows the history of the purchases of the user", Access(USER | ADMIN)),
+	Command("show -s", showStats, "shows the statistics of all bought products", Access::ADMIN),
+	Command("add -c", addProductToCart ,"add product to cart by id ", Access(GUEST | USER | ADMIN)),
 
 };
 
