e2d156883eb04b805758b3bba4d2168921955ee7
JulianaSen <yuliana.sen@gmail.com>
2015-05-04 15:29:36 +0300

    added function fill

--- firstProject1415/Products/Electronics/PhotoAndVideoCamera.cpp
+++ firstProject1415/Products/Electronics/PhotoAndVideoCamera.cpp
@@ -36,3 +36,35 @@ void PhotoAndVideoCamera::output() const
 	Electronics::output();
 	cout << Message("Megapixels: " + to_string(megapixels), LOG_MSG);
 }
+
+List<pair<string, string>> PhotoAndVideoCamera::metadata()
+{
+	List<pair<string, string>> result = Electronics::metadata();
+	result.pushBack(make_pair("megapixels", to_string(megapixels)));
+	return result;
+}
+
+void PhotoAndVideoCamera::fill(string source)
+{
+	this->Electronics::fill(source);
+	int posStart = 0;
+	int posEnd = source.find_first_of(',');
+	string field = "";
+	int i = 0, k = 0;
+	while (source[i] != '\n')
+	{
+		for (i = posStart; i < posEnd; i++)
+		{
+			field += source[i];
+		}
+		if(k == 7)
+		{
+			megapixels = stod(field);
+		}
+		field.clear();
+		k++;
+		posStart = posEnd + 1;
+		posEnd = source.find_first_of(',', posStart);
+		if (posEnd == string::npos) break;
+	}
+}
