c8b7ea549bb99c1b27c8411b5e9000678ed54dfb
JulianaSen <yuliana.sen@gmail.com>
2015-05-04 15:30:26 +0300

    Update LaptopAndComputer.h

--- firstProject1415/Products/Electronics/LaptopAndComputer.h
+++ firstProject1415/Products/Electronics/LaptopAndComputer.h
@@ -18,13 +18,6 @@ public:
 	virtual void input();
 	virtual void output() const;
 
-	virtual  List<pair<string, string>> metadata()
-	{
-		List<pair<string, string>> result = Electronics::metadata();
-		result.pushBack(make_pair("firm", firm));
-		return result;
-	}
-
 	virtual  List<pair<string, string>> metadata();
 
 	virtual void fill(string);
