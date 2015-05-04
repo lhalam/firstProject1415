e0ea19ebd163b4bf76172e94e0103c5c9af58b46
JulianaSen <yuliana.sen@gmail.com>
2015-05-04 15:27:02 +0300

    added function fill

--- firstProject1415/Products/Electronics/LaptopAndComputer.cpp
+++ firstProject1415/Products/Electronics/LaptopAndComputer.cpp
@@ -42,3 +42,35 @@ void LaptopAndComputer::output() const
 	Electronics::output();
 	cout << Message("Firm: " + firm, LOG_MSG);
 }
+
+List<pair<string, string>> LaptopAndComputer::metadata()
+{
+	List<pair<string, string>> result = Electronics::metadata();
+	result.pushBack(make_pair("firm", firm));
+	return result;
+}
+
+void LaptopAndComputer::fill(string source)
+{
+	this->Electronics::fill(source);
+	int posStart = 0;
+	int posEnd = source.find_first_of(',');
+	string field = "";
+	int i = 0, k = 0;
+	while (source[i] != '\n')
+	{
+		for (i = posStart; i < posEnd; i++)
+		{
+			field += source[i];
+		}
+		if(k == 7)
+		{
+			firm = field;
+		}
+		field.clear();
+		k++;
+		posStart = posEnd + 1;
+		posEnd = source.find_first_of(',', posStart);
+		if (posEnd == string::npos) break;
+	}
+}
