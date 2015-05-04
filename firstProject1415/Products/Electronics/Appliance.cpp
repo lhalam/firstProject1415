987ea5cf34877ab06bbe01641303f2423e5f8cf1
JulianaSen <yuliana.sen@gmail.com>
2015-05-04 15:24:45 +0300

    added function fill

--- firstProject1415/Products/Electronics/Appliance.cpp
+++ firstProject1415/Products/Electronics/Appliance.cpp
@@ -35,3 +35,35 @@ void Appliance::output() const
 	Electronics::output();
 	cout << Message("Height: " + to_string(height), LOG_MSG);
 }
+
+List<pair<string, string>> Appliance::metadata()
+{
+	List<pair<string, string>> result = Electronics::metadata();
+	result.pushBack(make_pair("height", to_string(height)));
+	return result;
+}
+
+void Appliance::fill(string source)
+{
+	this->Electronics::fill(source);
+	int posStart = 0;
+	int posEnd = source.find_first_of(',');
+	string field = "";
+	int i = 0, k = 0;
+	while (source[i] != '\n')
+	{
+		for (i = posStart; i < posEnd; i++)
+		{
+			field += source[i];
+		}
+		if(k == 7)
+		{
+			weight = stod(field);
+		}
+		field.clear();
+		k++;
+		posStart = posEnd + 1;
+		posEnd = source.find_first_of(',', posStart);
+		if (posEnd == string::npos) break;
+	}
+}
