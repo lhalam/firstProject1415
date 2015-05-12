092ddc0a6622b075d31578f828a081fe561d225f
TolikPylypchuk <pylypchuk.tolik@gmail.com>
2015-05-12 18:47:29 +0300

    Added unit tests for the HTMLService class

--- tests/TolikPylypchukTests.cpp
+++ tests/TolikPylypchukTests.cpp
@@ -1,16 +1,59 @@
 #include "stdafx.h"
 #include "CppUnitTest.h"
 
+#define DLL_IMPORT
+
+#include "DataManager/DataManager.h"
+#include "UI/UI.h"
+
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+using namespace std;
 
 namespace TolikPylypchukTests
 {
-	TEST_CLASS(Test)
+	TEST_CLASS(HTMLServiceTests)
 	{
-	public:
-		TEST_METHOD(TestMethod)
-		{
+		ifstream fin;
+		ofstream fout;
 
+	public:
+		HTMLServiceTests()
+		{
+			fin.open("input.txt");
+			if (!fin.is_open())
+			{
+				exit(EXIT_FAILURE);
+			}
+
+			filebuf *in = fin.rdbuf();
+			cin.rdbuf(in);
+
+			fout.open("output.txt");
+			if (!fin.is_open())
+			{
+				exit(EXIT_FAILURE);
+			}
+
+			filebuf *out = fout.rdbuf();
+			cout.rdbuf(out);
+		}
+
+		~HTMLServiceTests()
+		{
+			fin.close();
+			fout.close();
+		}
+		
+		TEST_METHOD(WriteTest)
+		{
+			currentUser = User("testname", "testsurname", Date(1, 1, 2000),
+				"test", "pass", "test@test.com", Access::USER, 4);
+
+			DataManager manager;
+			manager.writeInHTML(currentUser.getId(), Date(), Date());
+
+			//I have no idea what to check here
+			//I guess I just have to have a look on the file myself after the test
 		}
 	};
 }
