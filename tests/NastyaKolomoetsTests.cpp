80addb4b13249c364c9d85b8d760bf39ef243984
NastyaKolomoets <nastya.kolomoets@gmail.com>
2015-05-17 20:55:30 +0300

    Updated tests

--- tests/NastyaKolomoetsTests.cpp
+++ tests/NastyaKolomoetsTests.cpp
@@ -34,11 +34,40 @@ namespace NastyaKolomoetsTests
 
 		TEST_METHOD(ShowUsersTest)
 		{
+			stringstream sstream;
+			streambuf* coutstream = cout.rdbuf(sstream.rdbuf()); 
+			commands[21].execute();
+			cout.rdbuf(coutstream);
+			string output = sstream.str();
+			/*string line = "Name: Nastya\n"
+				"Surname: Kolomoets\n"
+				"Birthday: 12/5/1997\n"
+				"Login: Nastya\n"
+				"Password: nastya\n"
+				"Email: nastya.kolomoets@gmail.com\n"
+				"Role:  user\n"
+				"Id: 1\n"
+				"\nListing completed.\n";*/
+			string line = "Listing completed.\n";
+			Assert::AreEqual(output, line);
 			Assert::AreEqual(to_string(commands[21].execute().getId()), to_string(SUCCESSFUL));
 		}
 
 		TEST_METHOD(ShowStatsTest)
 		{
+			stringstream sstream;
+			streambuf* coutstream = cout.rdbuf(sstream.rdbuf());
+			commands[24].execute();
+			cout.rdbuf(coutstream);
+			string output = sstream.str();
+			/*string line = "Name: Shirt\n"
+				"Manufacturer: OGGI\n"
+				"Price: 350.000000\n"
+				"ID: 6\n"
+				"Quantity: 0\n"
+				"Listing completed.\n";*/
+			string line = "Listing completed.\n";
+			Assert::AreEqual(output, line);
 			Assert::AreEqual(to_string(commands[24].execute().getId()), to_string(SUCCESSFUL));
 		}
 	};
