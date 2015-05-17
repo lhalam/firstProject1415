c43f1cdd0ddf418e5df505e1dceadba252596d6d
NastyaKolomoets <nastya.kolomoets@gmail.com>
2015-05-17 19:00:18 +0300

    Tests

---
+++ tests/NastyaKolomoetsTests.cpp
@@ -0,0 +1,45 @@
+#include "stdafx.h"
+#include "CppUnitTest.h"
+
+#define DLL_IMPORT
+#include "UI/UI.h"
+#include "DataManager\DataManager.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace NastyaKolomoetsTests
+{		
+	TEST_CLASS(CommandHandlersTests)
+	{
+	public:
+		
+		TEST_METHOD(RemoveProdutFromAssortmentTest)
+		{
+			int id = 3;
+			string inputId = to_string(id) + "\n";
+			stringbuf in(inputId);
+			cin.rdbuf(&in);
+			DataManager manager;
+			try
+			{
+				commands[18].execute();
+			}
+			catch (...)
+			{
+				Assert::Fail();
+			}
+			Assert::AreEqual(-1, manager.getQuantity(id));
+			Assert::AreEqual(to_string(commands[18].execute().getId()), to_string(SUCCESSFUL));
+		}
+
+		TEST_METHOD(ShowUsersTest)
+		{
+			Assert::AreEqual(to_string(commands[21].execute().getId()), to_string(SUCCESSFUL));
+		}
+
+		TEST_METHOD(ShowStatsTest)
+		{
+			Assert::AreEqual(to_string(commands[24].execute().getId()), to_string(SUCCESSFUL));
+		}
+	};
+}
